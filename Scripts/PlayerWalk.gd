aedd3309d01333458e1cbf2ef3ad8dd90777dd98
Elias Veines-Jensen <eliasvj4312@gmail.com>
2026-01-30 21:50:13 +0100

    variables

--- Scripts/PlayerWalk.gd
+++ Scripts/PlayerWalk.gd
@@ -4,6 +4,11 @@ extends Node2D
 @export var leftLeg: RigidBody2D
 @export var rightLeg: RigidBody2D
 
+@export var kp := 1200.0
+@export var kd := 120.0
+@export var max_torque := 9000.0
+@export var rotation_power := 0.01
+
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
 	pass # Replace with function body.
